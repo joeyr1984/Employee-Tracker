5754725a648b4ca87fbc199a0265f2e015e7d71e
Joey Reyes <joeyr1984@gmail.com>
2020-06-23 18:59:22 -0700

    added switch statement and functions for viewing all departments and all roles

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
-var express = require("express");
 var mysql = require("mysql");
-
-
-var app = express();
-
-
-var PORT = process.env.PORT || 8080;
-
+var inquirer = require("inquirer");
+require("console.table")
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: ""
+  password: "password",
+  database: "employee_DB"
 });
+connection.connect(function (err) {
+  if (err) {
+    console.log(err);
+
+  }
+  console.log("connection id", connection.threadId);
+  displayMenu()
+})
+function displayMenu() {
+  inquirer.prompt({
+    type: "list",
+    message: "Please select one choice.",
+    choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update Employee Roles"],
+    name: "choice"
+  }).then(function (userInput) {
+    switch (userInput.choice) {
+      case "View All Departments":
+        viewAllDepartments();
+        break;
+      case "View All Roles":
+        viewAllRoles();
+        break;
+      case "View All Employees":
+        viewAllEmployees();
+        break;
+      case "Add a Department":
+        addDepartment();
+        break;
+      case "Add a Role":
+        addRole();
+        break;
+      case "Add an Employee":
+        addEmployee();
+        break;
+      case "Update Employee Roles":
+        updateEmployeeRoles();
+        break;
+    }
+  }
+  )
+}
+function viewAllDepartments() {
+  connection.query("SELECT * FROM department", function (err, results) {
+    console.table(results);
+    displayMenu()
+  })
+}
+function viewAllRoles() {
+  connection.query("SELECT * FROM employee_role", function (err, results) {
+    console.table(results);
+    displayMenu()
+  })
+}    
